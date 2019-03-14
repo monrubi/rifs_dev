fe57b7c3aeb8fc79b2e6fe3693bf55a4005ae214
monrubi <wunderber8@hotmail.com>
2019-03-13 20:13:12 -0600

    Se agregaron las clases Propiedad y Renter (en inglés por conflicto de claridad, se puede modificar)

---
+++ riffsApp/Renter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace riffsApp
+{
+    public class Renter
+    {
+        string nombre { get; set; }
+        string tel { get; set; }
+        int clave { get; set; }
+        //tendrá citas
+        //List<Cita> citas;
+
+        public Renter() { }
+
+        public Renter(string _nombre, string _tel)
+        {
+            nombre = _nombre;
+            tel = _tel;
+            generateClave();
+        }
+
+        private int generateClave()
+        {
+            //por ahora da 1 pero en cuanto esté la base se creará conforme esta
+            return 1;
+        }
+        
+    }
+}
---
+++ riffsApp/propiedad.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace riffsApp
+{
+    public class Propiedad
+    {
+        int clave;
+        string dir { get; set; }
+        List<String> servicios { get; set; }
+        List<Renter> rentadores { get; set; }
+        Boolean amueblado { get; set; }
+        float espacio { get; set; }
+        float precio { get; set; }
+        //tendrá citas
+        //List<Cita> citas;
+
+        public Propiedad(string _dir, Boolean _amueblado, float _precio)
+        {
+            dir = _dir;
+            amueblado = _amueblado;
+            servicios = new List<String>();
+            espacio = 0;
+            precio = _precio;
+            generateClave();
+        }
+
+        private int generateClave()
+        {
+            //por ahora da 1 pero en cuanto esté la base se creará conforme esta
+            return 1;
+        }
+
+
+
+    }
+}
