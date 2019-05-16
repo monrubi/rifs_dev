abfe152a084f4984c52305f7d7db713b91079dec
monrubi <wunderber8@hotmail.com>
2019-05-16 16:25:00 -0500

    creadas propiedades y convertiodas imagenes a botones

--- riffsApp/propiedad.cs
+++ riffsApp/propiedad.cs
@@ -20,20 +20,22 @@ namespace riffsApp
         float distancia { get; set; }
 
         string rutaImagen { get; set;}
+        string rutaColage { get; set; }
         //tendrá citas
         //List<Cita> citas;
 
 
-        public Propiedad(string _direccion, List<String> _serv, Boolean _amueblado, Boolean _transporte, float _precio, float _espacio, float _dist, string _rutaImagen)
+        public Propiedad(string _direccion, Boolean _amueblado, Boolean _transporte, float _precio, float _espacio, float _dist, string _rutaImagen, string _rutaColage)
         {
             direccion = _direccion;
             amueblado = _amueblado;
             transporte = _transporte;
-            servicios = _serv;
+            servicios = new List<string>();
             espacio = _espacio;
             precio = _precio;
             distancia = _dist;
             rutaImagen = _rutaImagen;
+            rutaColage = _rutaColage;
             clave = generateClave();
         }
 
@@ -41,6 +43,11 @@ namespace riffsApp
         {
             //por ahora da 1 pero en cuanto esté la base se creará conforme esta
             return clave++;
+        }
+
+        public void agrega_servicio(String _servicio)
+        {
+            servicios.Add(_servicio);
         }
 
 
