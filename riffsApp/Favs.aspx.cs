f3ac4b940b66bfcc1a943fdf4cd054ec0b2ea198
FMERINOB <FMERINOB@112SALAS23>
2019-05-16 21:01:17 -0500

    Añadición de codigo para funcionalidad favoritos

--- riffsApp/Favs.aspx.cs
+++ riffsApp/Favs.aspx.cs
@@ -11,7 +11,30 @@ namespace riffsApp
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (Session["fav1"].Equals(true))
+            {
+                ib1.Visible = true;
+            }
+
+            if (Session["fav2"].Equals(true))
+            {
+                ib2.Visible = true;
+            }
+
+            if (Session["fav3"].Equals(true))
+            {
+                ib3.Visible = true;
+            }
+
+            if (Session["fav4"].Equals(true))
+            {
+                ib4.Visible = true;
+            }
+
+            if (Session["fav5"].Equals(true))
+            {
+                ib5.Visible = true;
+            }
         }
     }
 }
